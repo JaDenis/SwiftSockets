3a6da5e800d54ee8dfbf30efd701e4fa697722c8
Frank Tan <frankctan@gmail.com>
2017-03-07 19:59:32 -0500

    Identify players using UUID

---
+++ Sources/String.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+extension String {
+
+    static func random(length: Int = 10) -> String {
+        let base = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        var randomString: String = ""
+
+        for _ in 0..<length {
+            let randomValue = arc4random_uniform(UInt32(base.characters.count))
+            randomString += "\(base[base.index(base.startIndex, offsetBy: Int(randomValue))])"
+        }
+        return randomString
+    }
+}
