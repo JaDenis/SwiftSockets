762d71b3cf26cfaeece2140b350cddd3d4373355
Frank Tan <frankctan@gmail.com>
2017-03-07 20:01:30 -0500

    Keep strings up to date

--- Sources/Strings.swift
+++ Sources/Strings.swift
@@ -1,7 +1,8 @@
 public struct Strings {
     static let usernameKey = "username"
     static let matchNameKey = "matchName"
-    static let typeKey = "type_"
+    static let playerUUIDKey = "playerUUID"
+    static let playerActionKey = "playerAction"
 
     static let jsonFindMatchType = "JsonFindMatch"
     static let jsonPlayerActionType = "JsonPlayerAction"
