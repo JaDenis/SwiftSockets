82aa084619e4c997866a66d5f6847084955b362f
Frank Tan <frankctan@gmail.com>
2017-03-07 20:02:34 -0500

    Decode JSON in the Player model

--- Sources/Player.swift
+++ Sources/Player.swift
@@ -1,12 +1,21 @@
 import Foundation
+import WebSockets
+import JSON
 
 /// Player is a reference type with value semantics.
-class Player {
+struct Player {
+    typealias UUID = String
     /// What is the player's name?
     let name: String
 
+    let uuid: Player.UUID
+
+    let matchID: String
+
     /// Cumulative history of moves.
     let moveHistory: [PlayerAction]
+
+    let socket: WebSocket
 
     /// Convenience variable to calculate player charges.
     var charges: Int {
@@ -14,20 +23,60 @@ class Player {
         return 1
     }
 
-    /// Convenience initializer for a new player.
-    convenience init(name: String) {
-        self.init(name: name, moveHistory: [])
-    }
-
-    private init(name: String, moveHistory: [PlayerAction]) {
+    init(name: String, matchID: String, uuid: Player.UUID, socket: WebSocket) {
         self.name = name
-        self.moveHistory = moveHistory
+        self.matchID = matchID
+        self.socket = socket
+        self.uuid = uuid
+        self.moveHistory = []
     }
 
-    /// Add a PlayerAction to player move history.
-    func add(_ playerAction: PlayerAction) -> Player {
-        return Player(name: name, moveHistory: [playerAction] + moveHistory)
+    init(name: String,
+         playerID: Player.UUID,
+         matchID: String,
+         socket: WebSocket,
+         moveHistory: [PlayerAction]) {
+            self.name = name
+            self.uuid = playerID
+            self.matchID = matchID
+            self.socket = socket
+            self.moveHistory = moveHistory
     }
+
+    // TODO: Fill this in later.
+    func calculateHealth(vsPlayer player: Player) -> Int {
+        return 0
+    }
+
+    static func decodeUUIDAndPlayerAction(fromJSON json: JSON) -> (Player.UUID, PlayerAction)?  {
+        guard let uuid = json.decode(key: Strings.playerUUIDKey),
+            let str = json.decode(key: Strings.playerActionKey),
+            let action = PlayerAction.decode(fromStr: str) else {
+                return nil
+        }
+        return (uuid, action)
+    }
+
+    /// Decode a new player from JSON.
+    static func initNewPlayer(fromJSON json: JSON, uuid: Player.UUID, socket: WebSocket) throws -> Player {
+        // Decode player and match name. These fields will be attached to every request.
+        guard let playerName = json.decode(key: Strings.usernameKey),
+            let matchName = json.decode(key: Strings.matchNameKey) else {
+                throw PlayerError.invalidUsernameOrMatchName
+        }
+
+        return Player(name: playerName, matchID: matchName, uuid: uuid, socket: socket)
+    }
+
+    func add(playerAction: PlayerAction, socket: WebSocket) -> Player {
+        return Player(name: self.name,
+                      playerID: self.uuid,
+                      matchID: self.matchID,
+                      socket: socket,
+                      moveHistory: self.moveHistory + [playerAction])
+    }
+}
+
 public enum PlayerError: Error {
     case jsonInvalidKeys
     case matchCombineError
