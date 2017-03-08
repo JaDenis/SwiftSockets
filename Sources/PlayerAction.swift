d6929f920c52eb6d26543f5aff7c4c7628793454
Frank Tan <frankctan@gmail.com>
2017-03-07 19:59:50 -0500

    Decode actions from json strings

--- Sources/PlayerAction.swift
+++ Sources/PlayerAction.swift
@@ -6,4 +6,14 @@ enum PlayerAction {
     case charge
     case shoot
     case steal
+
+    static func decode(fromStr str: String) -> PlayerAction? {
+        switch str {
+        case "block": return .block
+        case "charge": return .charge
+        case "shoot": return .shoot
+        case "steal": return .steal
+        default: return nil
+        }
+    }
 }
