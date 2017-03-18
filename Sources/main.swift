2804ceacc8f8af33de4f9f2a79a2c9e570993074
Frank Tan <frankctan@gmail.com>
2017-03-18 15:50:22 -0400

    Refactor send method

--- Sources/main.swift
+++ Sources/main.swift
@@ -12,28 +12,22 @@ drop.get("hello") { request in
 //typealias Username = String
 //typealias Payload = String
 //typealias Payloads = [Payload]
-
 //fileprivate var connections = [Username : Payloads]()
+
 
 typealias MatchName = String
 
-// TODO: Move this into a struct to track game state.
-/// Track matches based on match name.
-fileprivate var matches: [MatchName: [Player.UUID]] = [:]
-
-/// Track players based on username.
-fileprivate var players: [Player.UUID: Player] = [:]
-
+var gameModel = GameModel()
 
 // TODO: Support multiple players.
 /**
-Calculate health and charges for two players based on their collective move history.
-Output is `(p1Health, p2Health, p1Charge, p2Charge)`.
+ Calculate health and charges for two players based on their collective move history.
+ Output is `(p1Health, p2Health, p1Charge, p2Charge)`.
  */
 func calculateHealthAndCharges(p1: Player, p2: Player) -> (Int, Int, Int, Int) {
     let combinedHistory = Array(zip(p1.moveHistory, p2.moveHistory))
 
-    let charges = combinedHistory.reduce((0,0,0,0)) { (total, tup) in
+    let healthAndCharges = combinedHistory.reduce((0,0,0,0)) { (total, tup) in
         // Determine charges and health for this round.
         let (p1m, p2m) = tup
         var (p1h, p2h, p1c, p2c) = total
@@ -64,8 +58,29 @@ func calculateHealthAndCharges(p1: Player, p2: Player) -> (Int, Int, Int, Int) {
         return (p1h, p2h, p1c, p2c)
     }
 
-    return charges
+    return healthAndCharges
 }
+
+func send(updatedPlayer: Player, with gameModel: GameModel) {
+    guard let players = gameModel.getPlayers(in: updatedPlayer.matchID) else {
+        print("error sending \(updatedPlayer)")
+        return
+    }
+
+    let currentPlayerJson = updatedPlayer.encodePlayerToJsonPlayer()
+    let otherPlayersJson = players.map { $0.encodePlayerToJsonPlayer() }
+
+    let json = [currentPlayerJson] + otherPlayersJson
+
+    for p in players {
+        do {
+            try p.socket.send(json)
+        } catch(let err) {
+            print("unable to send json: ", err)
+        }
+    }
+}
+
 
 drop.socket("ws") { req, ws in
 
@@ -85,63 +100,32 @@ drop.socket("ws") { req, ws in
             print("received json: \(json)")
 
             // If we receive a uuid and action, then add that action to the player's move history.
-            if let (uuid, action) = Player.decodeUUIDAndPlayerAction(fromJSON: json),
-                let player = players[uuid] {
-                    players[uuid] = player.add(playerAction: action, socket: ws)
+            if let (uuid, action) = Player.decodeUUIDAndPlayerAction(fromJSON: json) {
+                let updatedPlayer = gameModel.add(action: action, socket: ws, toPlayerWithUUID: uuid)
+                print("updatedPlayer: \(updatedPlayer)")
+                send(updatedPlayer: updatedPlayer, with: gameModel)
 
-                //TODO: Notify all players of the updated action.
-
-            // If we don't receive a uuid, create a new player and add that to the match.
+                // If we don't receive a uuid, create a new player and add that to the match.
             } else {
-                let uuid = String.random()
-                let newPlayer = try Player.initNewPlayer(fromJSON: json, uuid: uuid, socket: ws)
-                let matchID = newPlayer.matchID
-                players[uuid] = newPlayer
-
-                // Track matches.
-                if matches[matchID]?.append(uuid) == nil {
-                    matches[matchID] = [uuid]
-                }
-
-                // We need to send info on the new player to all players in the match.
-
-                // Use the match name to find the player UUIDs.
-                if let matchPlayerUUIDs = matches[matchID] {
-
-                    // TODO: Use error handling.
-                    let matchPlayers = matchPlayerUUIDs.map { players[$0]! }
-
-                    // Encode all players.
-                    let playerJson = matchPlayers.map { $0.encodePlayerToJsonPlayer() }
-
-                    // Encode the new player.
-                    let newPlayerJson = newPlayer.encodePlayerToJsonPlayer()
-
-                    // New player information must be in the first position.
-                    let jsonMessages = [newPlayerJson] + playerJson
-
-                    // Send out new player details to each websocket connection.
-                    for player in matchPlayers {
-                        // TODO: Potential race condition? What if many new players join at the same time? How does vapor deal with that?
-                        try player.socket.send(jsonMessages)
-                    }
-
-                    print("sent to \(matchPlayers.count) clients:")
-                    print("outgoingjson: \(newPlayerJson)")
-                }
+                let newPlayer = try Player.initNewPlayer(fromJSON: json,
+                                                         uuid: String.random(),
+                                                         socket: ws)
+                gameModel.update(players: [newPlayer])
+                print("newPlayer: \(newPlayer)")
+                send(updatedPlayer: newPlayer, with: gameModel)
             }
         } catch {
             print("there was an error.")
         }
 
-//        print("players: \(players)")
-//        print()
-//        print("matches: \(matches)")
-        print()
+        print("")
     }
 }
 
-            // If a match with the same name already exists, then combine the two matches.
+
+
+
+// If a match with the same name already exists, then combine the two matches.
 //            if let existingMatch = matches[match.name] {
 //                matches[match.name] = try Match.combine(matches: [existingMatch, match])
 //            } else {
@@ -156,7 +140,7 @@ drop.socket("ws") { req, ws in
 
 
 
-        // Save username and payload to a dictionary.
+// Save username and payload to a dictionary.
 //        if let username = json.object?["username"]?.string,
 //            let payload = json.object?["payload"]?.string {
 //
